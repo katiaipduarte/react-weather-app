caa92e4987387866416eaac6637ddd814a2c9d8f
Katia Duarte <katiaipduarte@users.noreply.github.com>
2021-04-03 18:04:29 +0100

    feat: add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -14,39 +14,19 @@ function App() {
         </p>
         <span>
           <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
             React
           </a>
           <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://redux.js.org/" target="_blank" rel="noopener noreferrer">
             Redux
           </a>
           <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://redux-toolkit.js.org/" target="_blank" rel="noopener noreferrer">
             Redux Toolkit
           </a>
           ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://react-redux.js.org/" target="_blank" rel="noopener noreferrer">
             React Redux
           </a>
         </span>
