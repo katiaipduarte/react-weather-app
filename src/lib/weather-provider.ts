4c20126dc31d835cea4e3f93b691c9c67d810c51
Katia Duarte <katiaipduarte@users.noreply.github.com>
2021-04-07 22:23:29 +0100

    feat: updaste environments

--- src/lib/weather-provider.ts
+++ src/lib/weather-provider.ts
@@ -1,16 +1,14 @@
-import { useSelector } from 'react-redux';
 import { WeatherResponse } from '../interfaces/dto/weather-response';
 import { Weather } from '../interfaces/weather';
 import { handleRequest } from '../utils/handle-request';
 import { mapToWeatherInterface } from '../utils/weather-mapper';
 
 const WeatherProvider = () => {
-  const baseUrl = process.env.REACT_APP_WEATHER_API_URL?.replace(/\/+$/, '');
   const exclude = 'minutely,hourly,alerts';
 
   const getWeather = async (lat: number, lon: number): Promise<Weather> => {
     return await fetch(
-      `${baseUrl}/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=${exclude}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`,
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=${exclude}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`,
       {
         method: 'GET',
       },
