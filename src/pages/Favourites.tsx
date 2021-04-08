f67ea78cd1a59398a26b75687529954092d1e029
Katia Duarte <katiaipduarte@users.noreply.github.com>
2021-04-08 23:26:20 +0100

    fix: remove console.log

--- src/pages/Favourites.tsx
+++ src/pages/Favourites.tsx
@@ -12,7 +12,6 @@ const Favourites = () => {
 
   useEffect(() => {
     setLocations(favouritesLocations.favourites);
-    console.log(favouritesLocations.favourites);
   }, [favouritesLocations.favourites]);
 
   return (
